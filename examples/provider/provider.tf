562556839017aff85d2803cb7ffba76c6f08bc26
disc <a.hacicheant@gmail.com>
2021-09-06 16:57:04 +0300

    Added README.md

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -1,6 +1,54 @@
+terraform {
+  required_providers {
+    pritunl = {
+      version = "~> 0.0.1"
+      source  = "disc/pritunl"
+    }
+  }
+}
+
 provider "pritunl" {
-  url      = var.pritunl_url        # optionally use PRITUNL_URL env var
-  token    = var.pritunl_api_token  # optionally use PRITUNL_TOKEN env var
-  secret   = var.pritunl_api_secret # optionally use PRITUNL_SECRET env var
-  insecure = var.pritunl_insecure   # optionally use PRITUNL_INSECURE env var
+  url    = "https://localhost"
+  token  = "api-token"
+  secret = "api-secret"
+
+  insecure = false
+}
+
+resource "pritunl_organization" "developers" {
+  name = "Developers"
+}
+
+resource "pritunl_organization" "admins" {
+  name = "Admins"
+}
+
+resource "pritunl_user" "test" {
+  name            = "test-user"
+  organization_id = pritunl_organization.developers.id
+  email           = "test@test.com"
+  groups = [
+    "admins",
+  ]
+}
+
+resource "pritunl_server" "test" {
+  name = "test"
+
+  organization_ids = [
+    pritunl_organization.developers.id,
+    pritunl_organization.admins.id,
+  ]
+
+  route {
+    network = "10.0.0.0/24"
+    comment = "Private network #1"
+    nat     = true
+  }
+
+  route {
+    network = "10.2.0.0/24"
+    comment = "Private network #2"
+    nat     = false
+  }
 }
