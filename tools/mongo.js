92799af3dea8aa2587b42d27e281d5030a4e8f5d
disc <a.hacicheant@gmail.com>
2022-09-06 20:42:38 +0300

    Replaced type of SSO field to `interface{}` instead of `string|bool` because it could be bool (if SSO is disabled) or string (if sso is enabled).

--- tools/mongo.js
+++ tools/mongo.js
@@ -1,13 +1,1 @@
-use pritunl;
-db.administrators.updateOne(
-    {
-        "username": "pritunl"
-    },
-    {
-        $set: {
-            auth_api: true,
-            token: "tfacctest_token",
-            secret: "tfacctest_secret"
-        }
-    }
-)
+db.administrators.updateOne({"username": "pritunl"}, {$set: {auth_api: true, token: "tfacctest_token", secret: "tfacctest_secret"}});
