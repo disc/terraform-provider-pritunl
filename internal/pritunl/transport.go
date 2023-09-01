ee080c0d899d517ab83f3e56cf3d9c1e81c24f28
Lukmanul Hakim D <ldurachman@alamisharia.co.id>
2023-09-01 20:26:33 +0700

    update transport

--- internal/pritunl/transport.go
+++ internal/pritunl/transport.go
@@ -1,23 +1,13 @@
 package pritunl
 
 import (
-	"crypto/hmac"
-	"crypto/md5"
-	"crypto/sha256"
-	"encoding/base64"
-	"fmt"
 	"net/http"
 	"net/url"
 	"path"
-	"strconv"
-	"strings"
-	"time"
 )
 
 type transport struct {
 	underlyingTransport http.RoundTripper
-	apiToken            string
-	apiSecret           string
 	baseUrl             string
 }
 
@@ -32,24 +22,6 @@ func (t *transport) RoundTrip(req *http.Request) (*http.Response, error) {
 		req.URL = u
 	}
 
-	timestamp := strconv.FormatInt(time.Now().Unix(), 10)
-	timestampNano := strconv.FormatInt(time.Now().UnixNano(), 10)
-
-	nonceMac := hmac.New(md5.New, []byte(t.apiSecret))
-	nonceMac.Write([]byte(strings.Join([]string{timestampNano, req.URL.Path, t.apiToken}, "")))
-	nonce := fmt.Sprintf("%x", nonceMac.Sum(nil))
-	authString := strings.Join([]string{t.apiToken, timestamp, nonce, strings.ToUpper(req.Method), req.URL.Path}, "&")
-
-	mac := hmac.New(sha256.New, []byte(t.apiSecret))
-	mac.Write([]byte(authString))
-	signature := base64.StdEncoding.EncodeToString(mac.Sum(nil))
-
-	req.Header.Add("Auth-Token", t.apiToken)
-	req.Header.Add("Auth-Timestamp", timestamp)
-	req.Header.Add("Auth-Nonce", nonce)
-	req.Header.Add("Auth-Signature", signature)
-
 	req.Header.Add("Content-Type", "application/json")
-
 	return t.underlyingTransport.RoundTrip(req)
 }
