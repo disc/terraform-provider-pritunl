014f2474cb3e72307ed2cd8f896e1bf52445f215
disc <a.hacicheant@gmail.com>
2023-09-15 16:43:35 +0300

    Refactored tests for `pritunl_user`

--- internal/provider/resource_user_test.go
+++ internal/provider/resource_user_test.go
@@ -3,8 +3,6 @@ package provider
 import (
 	"fmt"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
-	"strings"
 	"testing"
 )
 
@@ -27,73 +25,8 @@ func TestAccPritunlUser(t *testing.T) {
 					Config: testPritunlUserConfig(username, orgName),
 					Check:  check,
 				},
-			},
-		})
-	})
-
-	t.Run("updates users without error", func(t *testing.T) {
-		username := "tfacc-user1"
-		orgName := "tfacc-org1"
-
-		newUsername := "tfacc-user1-new"
-
-		initialConfig := testPritunlUserConfig(username, orgName)
-
-		checks := map[string]resource.TestCheckFunc{
-			"before": resource.ComposeTestCheckFunc(
-				resource.TestCheckResourceAttr("pritunl_user.test", "name", username),
-				resource.TestCheckResourceAttr("pritunl_organization.test", "name", orgName),
-			),
-			"after": resource.ComposeTestCheckFunc(
-				resource.TestCheckResourceAttr("pritunl_user.test", "name", newUsername),
-				resource.TestCheckResourceAttr("pritunl_organization.test", "name", orgName),
-			),
-		}
-
-		resource.Test(t, resource.TestCase{
-			PreCheck:          func() { preCheck(t) },
-			ProviderFactories: providerFactories,
-			Steps: []resource.TestStep{
-				{
-					Config: initialConfig,
-					Check:  checks["before"],
-				},
-				{
-					Config: strings.Replace(initialConfig, username, newUsername, 1),
-					Check:  checks["after"],
-				},
-			},
-		})
-	})
-
-	t.Run("imports users without error", func(t *testing.T) {
-		username := "tfacc-user1"
-		orgName := "tfacc-org1"
-
-		check := resource.ComposeTestCheckFunc(
-			resource.TestCheckResourceAttr("pritunl_user.test", "name", username),
-			resource.TestCheckResourceAttr("pritunl_organization.test", "name", orgName),
-		)
-
-		resource.Test(t, resource.TestCase{
-			PreCheck:          func() { preCheck(t) },
-			ProviderFactories: providerFactories,
-			Steps: []resource.TestStep{
-				{
-					Config: testPritunlUserConfig(username, orgName),
-					Check:  check,
-				},
-				{
-					ResourceName:      "pritunl_user.test",
-					ImportState:       true,
-					ImportStateVerify: true,
-					ImportStateIdFunc: func(state *terraform.State) (string, error) {
-						userId := state.RootModule().Resources["pritunl_user.test"].Primary.Attributes["id"]
-						orgId := state.RootModule().Resources["pritunl_organization.test"].Primary.Attributes["id"]
-
-						return fmt.Sprintf("%s-%s", orgId, userId), nil
-					},
-				},
+				// import test
+				pritunlUserImportStep("pritunl_user.test"),
 			},
 		})
 	})
